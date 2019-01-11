56be60a55ee0285889167eee06e08d17c3087419
Srirakshith Betageri <sbetageri111@gmail.com>
2019-01-10 23:36:15 -0600

    Partial model built

--- Mask.py
+++ Mask.py
@@ -2,3 +2,27 @@ import torch
 import torch.nn
 import torch.nn.Functional as F
 
+from torchvision import models
+
+class MaskModel(torch.nn.Module):
+
+    OBJECTNESS_THRESHOLD = 0.7
+
+    def __init__(self, model, classifier):
+        self.model = model
+        self.classifier = classifier
+
+        self.catClassifier
+        self.regressor
+
+    def forward(self, x):
+        features = self.model(x)
+
+        # TODO
+        # Add ROI Features. Anchoring is also needed here.
+
+        RoIs = self.getRoI(x, features)
+        for RoI in RoIs:
+            objectnessScore = self.classifier(RoI)
+            if objectnessScore > MaskModel.OBJECTNESS_THRESHOLD:
+
